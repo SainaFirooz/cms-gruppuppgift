fe9350c37dd46faa0250550f8d8f1856ce40fbf9
Saina Firooz <saina.firooz@gmail.com>
2024-09-12 12:59:40 +0200

    robots.js done

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,17 @@
 export default function Robots() {
-    return {
-        rules: {
-          userAgent: '*',
-          allow: '/',
-          disallow: '/private/',
-        },
-        sitemap: '',
-      }
+  return {
+    rules: [
+      {
+        userAgent: "",
+        allow: "/",
+        disallow: "/api/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: ["/"],
+        disallow: "/private/",
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+  };
 }
