950327d17d0fb6139dc0e30d06f015b8d145d8f6
Saina Firooz <saina.firooz@gmail.com>
2024-09-12 13:00:20 +0200

    fix

--- src/components/nestable/Banner.jsx
+++ src/components/nestable/Banner.jsx
@@ -1,16 +1,13 @@
-
-
 const Banner = ({ blok }) => {
+  return (
+    <div>
+      <img
+        src={blok.background_image.filename}
+        alt=""
+        className="h-64 w-full object-cover"
+      />
+    </div>
+  );
+};
 
-    // console.log(blok);
-    //className="banner-container p-6 flex justify-center h-64" 
-
-    return(
-        <div >
-            <img src={blok.background_image.filename} alt=""  className="h-64 w-full object-cover" />
-        </div>
-
-    )
-}
-
-export default Banner
+export default Banner;
