80ae0a09a2b4fdea991ee48b6ede7757e6ca86c2
Saina Firooz <saina.firooz@gmail.com>
2024-09-12 13:02:22 +0200

    padding

--- src/components/nestable/Image.jsx
+++ src/components/nestable/Image.jsx
@@ -1,12 +1,9 @@
 const Image = ({ blok }) => {
+  return (
+    <div className="pl-90px pr-90px  pb-20px flex justify-center">
+      <img src={blok?.image.filename} alt="" className="rounded-lg" />
+    </div>
+  );
+};
 
-    // console.log(blok);
-
-    return(
-        <div className="p-10 flex justify-center  ">
-            <img src={blok?.image.filename} alt=""  className="rounded-lg" />
-        </div>
-    )
-}
-
-export default Image
+export default Image;
